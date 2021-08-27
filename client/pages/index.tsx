a0fedabf8af76c0ff155f84935012349b717711c
navneethvariar14 <57003649+navneethvariar14@users.noreply.github.com>
2021-08-27 23:27:16 +0530

    Add tabs

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay } from "swiper";
 // icons
@@ -14,6 +15,10 @@ import styles from "../styles/Home.module.css";
 SwiperCore.use([Autoplay]);
 
 const Home: NextPage = () => {
+  const [selected, setSelected] = useState<number>(1);
+
+  const filters = ["All", "Short Story", "Poem", "Quote"];
+
   return (
     <>
       <Seo title="Cryptoliterature" description="Crypto literature" />
@@ -68,52 +73,106 @@ const Home: NextPage = () => {
           </SwiperSlide>
         </Swiper>
       </section>
-      <Card
-        genre="Poem"
-        hash="#473658"
-        title="It's a nice good evening"
-        avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-        name="Lara Clarke"
-        currentBid="50 ETH"
-        endingIn="05h 12m 45s"
-      />
-      <Card
-        genre="Poem"
-        hash="#473658"
-        title="It's a nice good evening"
-        avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-        name="Lara Clarke"
-        soldFor="50 ETH"
-        ownedBy="James Hood"
-        ownedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-      />
-      <Card
-        genre="Poem"
-        hash="#473658"
-        title="It's a nice good evening"
-        avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-        name="Lara Clarke"
-        uploadedIn="5 June 2020"
-      />
-      <Card
-        genre="Poem"
-        hash="#473658"
-        title="It's a nice good evening"
-        avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-        name="Lara Clarke"
-        ownedBy="James Hood"
-        ownedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-      />
-      <Card
-        genre="Poem"
-        hash="#473658"
-        title="It's a nice good evening"
-        avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-        name="Lara Clarke"
-        reservePrice="50 ETH"
-        listedBy="James Maxwell"
-        listedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-      />
+      <div className="container mx-auto xl:px-24">
+        <div className="flex pl-4 md:justify-start my-5 justify-center text-lit-dark">
+          {filters.map((filter, i) => (
+            <button
+              key={`filter-${i}`}
+              onClick={() => {
+                setSelected(i);
+              }}
+              className={`mx-2 px-4 p-1 rounded-full transition ease-linear ${
+                selected == i ? "bg-lit-dark text-white" : ""
+              }`}
+            >
+              <span>{filter}</span>
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-self-center gap-3 gap-y-5">
+          <Card
+            selected={filters[selected]}
+            genre="Poem"
+            hash="#473658"
+            title="The best way to predict the future"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            currentBid="5.00 ETH"
+            endingIn="05h 12m 45s"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Quote"
+            hash="#473658"
+            title="The best way to predict the furture"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            soldFor="5.00 ETH"
+            ownedBy="James Hood"
+            ownedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Poem"
+            hash="#473658"
+            title="The best way to predict the furture"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            uploadedIn="5 June 2020"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Short Story"
+            hash="#473658"
+            title="The best way to predict the future"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            ownedBy="James Hood"
+            ownedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Quote"
+            hash="#473658"
+            title="The best way to predict the future"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            reservePrice="5.00 ETH"
+            listedBy="James Maxwell"
+            listedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Short Story"
+            hash="#473658"
+            title="The best way to predict the future"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            soldFor="5.00 ETH"
+            ownedBy="James Hood"
+            ownedByAvatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Short Story"
+            hash="#473658"
+            title="The best way to predict the future"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            uploadedIn="5 June 2020"
+          />
+          <Card
+            selected={filters[selected]}
+            genre="Poem"
+            hash="#473658"
+            title="The best way to predict the future"
+            avatar="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
+            name="Lara Clarke"
+            currentBid="5.00 ETH"
+            endingIn="05h 12m 45s"
+          />
+        </div>
+      </div>
     </>
   );
 };
